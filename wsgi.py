05ba793709930c94bd6f3d350bcc69e44f94a28d
Noah Homyak <noahhomyak@Mac.attlocal.net>
2025-06-27 01:18:28 -0400

    updated env file locations

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,8 @@ gevent.monkey.patch_all()
 
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv(Path(".env"))
-load_dotenv(Path(".env.prod"), override=True)
+load_dotenv(Path("/home/deploy/env-files/.env"), override=True)
+load_dotenv(Path("/home/deploy/env-files/.env.prod"), override=True)
 
 from app import create_app
 app = create_app()
